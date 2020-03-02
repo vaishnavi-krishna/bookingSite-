6d9338818d33facd147daa7a06c84925adbe3afa
vaishnavi6699 <59757016+vaishnavi6699@users.noreply.github.com>
2020-03-02 18:55:41 +0530

    Update and rename server.js to app.js

---
+++ app.js
@@ -0,0 +1,85 @@
+
+//Importing the pacakges.....
+
+const express= require('express');
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+const path = require('path');
+
+//Creating database and collections
+
+const db = require("./db");
+const collection = "credentials";
+
+//Redirection to index.html file
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+
+//Display the data....
+
+app.get('/getResult',(req,res)=>{
+     db.getDB().collection(collection).find({}).toArray((err,documents)=>{
+         if(err)
+           console.log(err);
+        else{
+            console.log(documents);
+            res.json(documents);
+        }
+     });
+});
+
+//update the Data...
+
+app.put('/:id',(req,res)=>{
+     const userID = req.params.id;
+     const userInput = req.body;
+
+     db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(userID)},{$set : {user : userInput.user}},{returnValue : false},(err,result)=>{
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            res.json(result);
+        }
+     });
+
+});
+
+
+//Insert the Data...
+
+app.post('/',(req,res)=>{
+    const userInput = req.body;
+    db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+      if(err)
+      {
+          console.log(err);
+      }
+      else{
+          res.json({result : result,document : result.ops[0]});
+      }
+    });
+
+});
+
+//Connecting and verification of Database
+
+db.connect((err)=>{
+    if(err)
+    {
+        console.log('Opps!!! unable to connect to database');
+        process.exit(1);
+
+    }
+    else
+    {
+        app.listen(8080,()=>{
+            console.log('Connected to database, current port number is 8080');
+           
+        });
+    }
+})
